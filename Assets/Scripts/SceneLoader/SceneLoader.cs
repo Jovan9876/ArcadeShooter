118798af6bdf27b07f7bad0e0e496d4e127fcc63
Jovan9876 <jovansandhu3@gmail.com>
2025-03-07 18:21:10 -0800

    Added pause menu to go back to mainmenu

--- Assets/Scripts/SceneLoader/SceneLoader.cs
+++ Assets/Scripts/SceneLoader/SceneLoader.cs
@@ -4,6 +4,16 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour {
 
+    public GameObject pauseMenu;
+
+    private void Start() {
+        Time.timeScale = 1f;
+
+        if (pauseMenu != null) {
+            pauseMenu.SetActive(false);
+        }
+    }
+
     public void LoadScene(string newScene) {
         StartCoroutine(LoadSceneAsync(newScene));
     }
@@ -19,7 +29,19 @@ public class SceneLoader : MonoBehaviour {
         while (!asyncLoad.isDone) {
             yield return null;
         }
-
     }
+
+
+    public void ShowPauseMenu() {
+        if (pauseMenu.activeSelf) {
+            Time.timeScale = 1f;
+            pauseMenu.SetActive(false);
+        } else {
+            Time.timeScale = 0f;
+            pauseMenu.SetActive(true);
+        }
+    }
+
+
 
 }
