352fa8a8c103fd73c86afe5519e70748ee3affae
Jerry Han <jerry000@hotmail.ca>
2025-04-01 13:43:48 -0700

    pulling from dev

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
+    //Enemy damage
+    public int damageAmount = 0;
+
+    public int maxMobHealth = 100;
+    public int currentHealth;
+
+    public int mobExp = 25;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+     currentHealth = maxMobHealth;   
     }
 
     // Update is called once per frame
@@ -13,4 +22,17 @@ public class EnemyManager : MonoBehaviour
     {
         
     }
+
+    public void TakeDamage(int damage){
+        currentHealth -= damage;
+
+        if(currentHealth <= 0) {
+            deathSequence();
+        }
+    }
+
+    private void deathSequence(){
+        
+    }
+
 }
