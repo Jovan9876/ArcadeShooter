fa7c8a693d707023e1e5959a4c4e394063893472
s-Kone <Connie-jay@hotmail.com>
2025-04-14 18:09:42 -0700

    camera lock and bounds

--- Assets/PlaneScroll.cs
+++ Assets/PlaneScroll.cs
@@ -3,11 +3,25 @@ using UnityEngine;
 
 public class PlaneScroll : MonoBehaviour
 {
-    public float scrollSpeed = -5f;
+    public float scrollSpeed = 2.5f;
+
+    // Set bounds
+    public float minX = 100f;
+    public float maxX = 180f;
+    public float minZ = -370f;
+    public float maxZ = -40f;
+
     void Update()
     {
         Vector3 currentPosition = transform.position;
+
+        // Scroll on z-axis
         currentPosition.z -= scrollSpeed * Time.deltaTime;
+
+        // Clamp x and z positions
+        currentPosition.x = Mathf.Clamp(currentPosition.x, minX, maxX);
+        currentPosition.z = Mathf.Clamp(currentPosition.z, minZ, maxZ);
+
         transform.position = currentPosition;
     }
 }
